44633334b6427a39c421f31637f0521f7fa3f585
EGWiseMacbook <user@eg-work-macbook-pro.local>
2024-12-17 13:30:20 +0100

    Initial commit

---
+++ TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTaskLvl2_E_Harbuzou
+//
+//  Created by EGWiseMacbook  on 17/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
