d3cfc0f74f0abca0f385b4d21ed46b05329ee3c0
E_Harbuzou <>
2024-12-17 15:43:44 +0100

    refactor: setup app via app delegate flow
    - add coordinator

--- TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/Application/AppDelegate.swift
+++ TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/Application/AppDelegate.swift
@@ -1,10 +1,20 @@
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = Configurator.configure()
+        let navigation = UINavigationController(rootViewController: rootViewController)
+        
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
         
         return true
     }
 }
-
---
+++ TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/Application/Configurator.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class Configurator {
+    static func configure() -> UIViewController {
+        let AuthViewController = AuthViewController()
+        return AuthViewController
+    }
+}
