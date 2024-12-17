75ebd88326afb136090cd3658dde6cc92ef2bf38
E_Harbuzou <>
2024-12-17 14:01:36 +0100

    feat: project set up
    - remove landscape mode
    - disabled appearance
    - add app icon
    - managed files hierarchy
    - removed main storyboard
    - add git ignore

--- TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/ViewController.swift
+++ TechnicalTaskLvl2_E_Harbuzou/TechnicalTaskLvl2_E_Harbuzou/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  TechnicalTaskLvl2_E_Harbuzou
-//
-//  Created by EGWiseMacbook  on 17/12/2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
